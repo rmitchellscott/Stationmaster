26b1fc81b60236f3b7dd8526808dd549cc0a5285
Mitchell Scott <10804314+rmitchellscott@users.noreply.github.com>
2025-08-21 12:30:18 -0600

    feat: add screenshot plugin

--- main.go
+++ main.go
@@ -36,8 +36,10 @@ import (
 	// Plugin imports for auto-registration
 	_ "github.com/rmitchellscott/stationmaster/internal/plugins/alias"
 	_ "github.com/rmitchellscott/stationmaster/internal/plugins/core_proxy"
+	_ "github.com/rmitchellscott/stationmaster/internal/plugins/days_left"
 	_ "github.com/rmitchellscott/stationmaster/internal/plugins/image_display"
 	_ "github.com/rmitchellscott/stationmaster/internal/plugins/redirect"
+	_ "github.com/rmitchellscott/stationmaster/internal/plugins/screenshot"
 )
 
 //go:generate npm --prefix ui install
